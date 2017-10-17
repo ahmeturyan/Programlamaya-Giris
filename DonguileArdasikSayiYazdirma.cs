00a1c0da228fe5b4c42259255d016fc1965fe463
Ahmet Uryan <32760691+ahmeturyan@users.noreply.github.com>
2017-10-17 20:37:04 +0300

    DonguileArdasikSayiYazdirma.cs

---
+++ DonguileArdasikSayiYazdirma.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonguileArdasikSayiYazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for(int i=0; i<100; i++)
+            {
+                Console.WriteLine(i);
+             
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
