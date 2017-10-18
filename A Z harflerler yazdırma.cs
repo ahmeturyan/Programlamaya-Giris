555fc37cbbdac7cb5fe87c595e66a86377f7c90e
Ahmet Uryan <32760691+ahmeturyan@users.noreply.github.com>
2017-10-18 11:20:31 +0300

    DonguileAdanZyeKadarYazdirma.cs

---
+++ A Z harflerler yazdırma.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZbutunHarfleriYazma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char i;
+            for (i = 'a'; i < 'z'; i++)
+                Console.WriteLine(i);
+            Console.ReadKey();
+
+        }
+    }
+}
